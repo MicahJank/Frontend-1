95223d07f791d3e5bf5df79dc91fb08790f6a614
JonathanChen <jonny29009@yahoo.com>
2019-12-28 13:46:05 -0800

    update file structure and add comments for to dos

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import PrivateRoute from './utils/PrivateRoute';
-import HomePage from './webpages/HomePage';
-import LoginPage from './webpages/LoginPage';
-import RegisterPage from './webpages/RegisterPage';
-import DashStudent from './webpages/DashStudent';
-import DashHelper from './webpages/DashHelper';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import DashStudent from './components/DashStudent';
+import DashHelper from './components/DashHelper';
 import './App.css';
 
 function App() {
@@ -21,7 +21,11 @@ function App() {
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
-      {/* Find a way to better control extra user type cases */}
+      {/* 
+        Find a way to better control how to reach target dashboard;
+        Extra user type cases: what if back end adds an extra user type 
+        which is not reflected in front end? Null is not a default type here
+      */}
       <PrivateRoute exact path="/dashboard" component={() => {
         switch(userType) {
           case "Student": 

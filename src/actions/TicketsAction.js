ca30d82acb14df944a6a5b2653f0ed70156ab8ca
JonathanChen <jonny29009@yahoo.com>
2019-12-27 21:31:21 -0800

    add tickets reducer

---
+++ src/actions/TicketsAction.js
@@ -0,0 +1,5 @@
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+export const TICKETS_LOADING = 'TICKETS_LOADING';
+export const TICKETS_SUCCESS = 'TICKETS_SUCCESS';
+export const TICKETS_FAILURE = 'TICKETS_FAILURE';

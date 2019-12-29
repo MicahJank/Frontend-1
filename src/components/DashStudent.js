9f11eb1a6634ab1154f5746d477af2e816e048a9
JonathanChen <jonny29009@yahoo.com>
2019-12-28 21:02:25 -0800

    add a form to submit ticket

--- src/components/DashStudent.js
+++ src/components/DashStudent.js
@@ -17,13 +17,19 @@ function DashStudent() {
     dispatch(getTickets());
   }, [dispatch])
 
+  const onSubmit = data => {
+    console.log("Starting to submit a ticket", data);
+    dispatch(postTicket(data));
+  }
+
   return (
     <div>
       <h1>Student's homepage goes here.</h1>
       <Link to="/"><button>Go Back Home</button></Link>
       <div className="form-body">
       <h1 className="form-h1">Submit A Ticket</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        {/* Submitting a ticket: Title */}
         <label className="form-label">Title</label>
         <input 
           name="title" 
@@ -31,13 +37,31 @@ function DashStudent() {
           className="form-input"
         />
         {errors.title && <p className="form-p">A title is required!</p>}
+        {/* Submitting a ticket: Category */}
         <label className="form-label">Category</label>
-          <select name="category" ref={register({ required: true })}>
+          <select name="category_id" ref={register({ required: true })}>
             <option value="1">Applied Javascript</option>
             <option value="2">Intro to React</option>
             <option value="3">HTML Fundamentals</option>
           </select>
-        {errors.category && <p className="form-p">Please select a categor!</p>}
+        {errors.category && <p className="form-p">Please select a category!</p>}
+        {/* Submitting a ticket: Description */}
+        <label className="form-label">Description</label>
+        <input
+          name="description"
+          ref={register({ required: true })}
+          className="form-input"
+        />
+        {errors.description && <p className="form-p">Please enter a description.</p>}
+        {/* Submitting a ticket: Attempt */}
+        <label className="form-label">Attempt</label>
+        <input
+          name="tried"
+          ref={register({ required: true })}
+          className="form-input"
+        />
+        {errors.attempt && <p className="form-p">Tell us if this is your first attempt, second attempt, etc.</p>}
+        {/* Submitting the ticket */}
         <input type="submit" />
       </form>
       </div>

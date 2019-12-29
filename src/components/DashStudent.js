96191cfe29f81cec8d617a4635706a78fa274e22
JonathanChen <jonny29009@yahoo.com>
2019-12-28 17:51:23 -0800

    create ticketlist component and add to DashStudent ticket submission form

--- src/components/DashStudent.js
+++ src/components/DashStudent.js
@@ -3,24 +3,44 @@
 * It will contain a list of student's tickets
 **/
 
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
 import { Link } from 'react-router-dom';
+import useForm from 'react-hook-form';
 import { getTickets, postTicket } from '../actions/TicketsAction';
 
 function DashStudent() {
   const dispatch = useDispatch();
+  const { register, handleSubmit, errors } = useForm(); 
 
-  const getData = () => {
-    console.log("Staring");
+  useEffect(() => {
     dispatch(getTickets());
-  }
+  }, [dispatch])
 
   return (
     <div>
       <h1>Student's homepage goes here.</h1>
-      <button onClick={getData}>Click Me</button>
       <Link to="/"><button>Go Back Home</button></Link>
+      <div className="form-body">
+      <h1 className="form-h1">Submit A Ticket</h1>
+      <form className="form">
+        <label className="form-label">Title</label>
+        <input 
+          name="title" 
+          ref={register({ required: true })} 
+          className="form-input"
+        />
+        {errors.title && <p className="form-p">A title is required!</p>}
+        <label className="form-label">Category</label>
+          <select name="category" ref={register({ required: true })}>
+            <option value="1">Applied Javascript</option>
+            <option value="2">Intro to React</option>
+            <option value="3">HTML Fundamentals</option>
+          </select>
+        {errors.category && <p className="form-p">Please select a categor!</p>}
+        <input type="submit" />
+      </form>
+      </div>
     </div>
   );
 }

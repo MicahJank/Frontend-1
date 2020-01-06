acb9875d6fd462ebc285d0be5cc1ca13a02aff35
nicbongo <nicbongo@protonmail.com>
2020-01-06 14:33:02 -0400

    mapping attempt

---
+++ src/components/helper/HelpItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelpItem(props) {
+  return (
+    <div>asdas
+      {/* <p>{ticket.student_first_name}</p> */}
+    </div>
+  )
+}
+
+export default HelpItem
--- src/components/helper/HelpList.js
+++ src/components/helper/HelpList.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../../utils/axiosWithAuth';
 
+import HelpItem from "./HelpItem";
+
+
 const HelperList = () => {
+    const [tickets, setTickets] = useState([])
+
     useEffect(() => {
         axiosWithAuth()
         .get("/tickets")
         .then(res => {
             console.log("Getting response data", res.data);
+            setTickets(res.data)
         })
         .catch(err => {
             console.log(err);
         })
-    })
+    }, [])
 
     return(
         <div>
             <h2>Welcome to random places</h2>
+            {tickets.map(ticket => {
+                <HelpItem 
+                    ticket={ticket} 
+                    key={ticket.id} 
+                />
+                })          
+            }
         </div>
     )
 }

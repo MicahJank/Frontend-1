5b6b0a57e631a72be08351c2fdebb4564e7875b7
nicbongo <nicbongo@protonmail.com>
2020-01-08 23:31:28 -0400

    included font-awesome dependencies

--- src/Style/HelperStyle.js
+++ src/Style/HelperStyle.js
@@ -16,6 +16,11 @@ export const TicketCard = styled.div`
   background-color: #FEFEFE;  
   border: 1px solid #423F40;
   padding: 5%;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    &: hover {
+      box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+      cursor: grab;
+  }
 `;
 
 
